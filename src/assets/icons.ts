6dfd603094705e5f9dc3f1238d5520c91447c3c9
BrianWong05 <wongtiniaomacau@gmail.com>
2025-02-15 01:09:34 +0800

    chore: change icons.ts location

---
+++ src/assets/icons.ts
@@ -0,0 +1,40 @@
+import bugIcon from './images/icons/bug.svg';
+import darkIcon from './images/icons/dark.svg';
+import dragonIcon from './images/icons/dragon.svg';
+import electricIcon from './images/icons/electric.svg';
+import fairyIcon from './images/icons/fairy.svg';
+import fightingIcon from './images/icons/fighting.svg';
+import fireIcon from './images/icons/fire.svg';
+import flyingIcon from './images/icons/flying.svg';
+import ghostIcon from './images/icons/ghost.svg';
+import grassIcon from './images/icons/grass.svg';
+import groundIcon from './images/icons/ground.svg';
+import iceIcon from './images/icons/ice.svg';
+import normalIcon from './images/icons/normal.svg';
+import poisonIcon from './images/icons/poison.svg';
+import psychicIcon from './images/icons/psychic.svg';
+import rockIcon from './images/icons/rock.svg';
+import steelIcon from './images/icons/steel.svg';
+import waterIcon from './images/icons/water.svg';
+
+// export all icons
+export const icons = {
+  bug: bugIcon,
+  dark: darkIcon,
+  dragon: dragonIcon,
+  electric: electricIcon,
+  fairy: fairyIcon,
+  fighting: fightingIcon,
+  fire: fireIcon,
+  flying: flyingIcon,
+  ghost: ghostIcon,
+  grass: grassIcon,
+  ground: groundIcon,
+  ice: iceIcon,
+  normal: normalIcon,
+  poison: poisonIcon,
+  psychic: psychicIcon,
+  rock: rockIcon,
+  steel: steelIcon,
+  water: waterIcon,
+};
