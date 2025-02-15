46a874646c7fd2120f0017030e0a862c946ef82e
BrianWong05 <wongtiniaomacau@gmail.com>
2025-02-15 21:47:26 +0800

    feat: add search bar to move list

--- src/components/MoveList.tsx
+++ src/components/MoveList.tsx
@@ -1,14 +1,25 @@
+import { useState } from "react";
 import Move from "./Move";
+import SearchBar from "./SearchBar";
 
 function MoveList({gens}) {
   const moves = Array.from(gens.get(9).moves);
-  console.log(moves);
+  
+  const [searchResults, setSearchResults] = useState(moves);
+
+  const handleSearch = (query: string) => {
+    const filteredResults = moves.filter((move) =>
+      move.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  };
 
   return (
     <div className="bg-[#24283B]">
     <h2 className="text-3xl text-center text-gray-200 h-25 pt-10">Moves</h2>
+    <SearchBar onSearch={handleSearch}/>
     <div className="flex flex-wrap justify-center">
-      {moves.map((move) => {
+      {searchResults.map((move) => {
         return <Move move={move} />
       })};
     </div>
