08fdbeacc032423938166ff57d559011408e1379
BrianWong05 <wongtiniaomacau@gmail.com>
2025-02-16 23:35:39 +0800

    feat: preload custom tailwind css color

---
+++ src/components/Preload.tsx
@@ -0,0 +1,8 @@
+function Perload() {
+  return (
+    <div className="bg-bug bg-dark bg-dragon bg-electric bg-fairy bg-fighting bg-fire bg-flying bg-ghost bg-grass bg-ground bg-ice bg-normal bg-poison bg-psychic bg-rock bg-steel bg-water">
+    </div>
+  )
+}
+
+export default Perload
