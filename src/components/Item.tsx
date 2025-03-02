5a5e85e94fcdb6aac88e0f7e6f30ca0594e32d87
BrianWong05 <wongtiniaomacau@gmail.com>
2025-03-02 14:38:40 +1100

    style: resize the items

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -10,8 +10,10 @@ function Item({ item, onData }) {
 
   return (
     <div onClick={selectedItem}>
-      <div className={`text-gray-200 flex w-xl`}>
-        <img src={imagePath} />
+      <div className={`text-gray-200 flex w-xl h-20`}>
+        <div className="h-20 w-30 flex">
+          <img src={imagePath} className="max-h-20 px-2 m-auto" />
+        </div>
         <div className="text-2xl my-auto">{name}</div>
       </div>
       <hr className="w-xl border-gray-700" />
